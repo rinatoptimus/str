9d4d2ec579d6a7b03c1df83a58474aa1d641e018
rinat_y <qwwert@mail.rui>
2016-12-07 18:34:48 +0300

    Код стики сайдбара вынесен в отельные файлы.

---
+++ my-linker-2 (Страница клиники) (копия)/app/js/sticky-sidebar.js
@@ -0,0 +1,16 @@
+/**
+ * Created by rinat_y on 07.12.16.
+ */
+$('#sidebar').affix({
+    offset: {
+        top: 100
+    }
+});
+
+var $body   = $(document.body);
+var navHeight = $('.navbar').outerHeight(true) + 10;
+
+$body.scrollspy({
+    target: '#leftCol',
+    offset: navHeight
+});
