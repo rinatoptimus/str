99134feec9f60a1856ece18d5032c5bf62639b38
rinat_y <qwwert@mail.rui>
2016-12-08 09:07:40 +0300

    08.12.16 09:07 Код  попапов вынесен в отдельные файлы.

---
+++ my-linker-2 (Страница клиники) (копия)/app/js/popup-clinics.js
@@ -0,0 +1,52 @@
+/**
+ * Created by rinat_y on 08.12.16.
+ */
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+var modal2 = document.getElementById('myModal2');
+var img2 = document.getElementById('myImg2');
+var modalImg2 = document.getElementById("img02");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+    modal2.style.display = "block";
+    modalImg2.src = this.src;
+    captionText2.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close2")[0];
+span.onclick = function() {
+    modal2.style.display = "none";
+}
+
+
+var modal3 = document.getElementById('myModal3');
+var img3 = document.getElementById('myImg3');
+var modalImg3 = document.getElementById("img03");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function(){
+    modal3.style.display = "block";
+    modalImg3.src = this.src;
+    captionText3.innerHTML = this.alt;
+}
+var span = document.getElementsByClassName("close3")[0];
+span.onclick = function() {
+    modal3.style.display = "none";
+}
